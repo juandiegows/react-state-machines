a172b11e29bb55f83c226ad7f6987fdc5435575b
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2025-12-22 23:35:34 -0500

    deploy en git

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/react-state-machines/',
   server: {
-    port: 5173, // Para mantener el puerto que usaba React
+    port: 3000, // Para mantener el puerto que usaba React
     open: true  // Para que se abra el navegador automáticamente
   }
 })
